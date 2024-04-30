f1bebab6b69f2309922c92b1a336c81ff87badc9
Kate <mitsutanaka29@gmail.com>
2024-04-30 15:51:24 +0300

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@conarti/feature-sliced/recommended",
+  ],
+  ignorePatterns: [".yarn", "build", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "react/prop-types": 0,
+  },
+};
